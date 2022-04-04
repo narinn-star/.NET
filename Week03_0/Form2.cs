cc30a2090f7e9ba614f866c7bf753d2a20ed3a8c
narinn-star <skfls2618@naver.com>
2022-04-04 23:18:34 +0900

    22.04.04

---
+++ Week03_0/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week03_0
+{
+    public partial class Form2 : Form
+    {
+        private Color iDialogColor;
+
+        public Color Color
+        {
+            get
+            {
+                if (radioButton1.Checked) iDialogColor = Color.Red;
+                if (radioButton2.Checked) iDialogColor = Color.Green;
+                if (radioButton3.Checked) iDialogColor = Color.Blue;
+                return iDialogColor;
+            }
+            set
+            {
+                iDialogColor = value;
+                if (iDialogColor == Color.Red) radioButton1.Checked = true;
+                if (iDialogColor == Color.Green) radioButton2.Checked = true;
+                if (iDialogColor == Color.Blue) radioButton3.Checked = true;
+            }
+        }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
