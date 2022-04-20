5b9c09a5bf337eaa2608219ff5bc843b12481c00
narinn-star <skfls2618@naver.com>
2022-04-20 23:52:07 +0900

    22.04.20

---
+++ Week05_1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DrawLines
+{
+    public partial class Form1 : Form
+    {
+        private LinkedList<CMyData> total_lines;
+        CMyData data;
+        private int x, y;
+        private Color CurrentPenColor;
+        private int iCurrentPenWidth;
+        private bool doubleclick = false;
+        public Form1()
+        {
+            total_lines = new LinkedList<CMyData>();
+            CurrentPenColor = Color.Black;
+            iCurrentPenWidth = 2;
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                x = e.X;
+                y = e.Y;
+                data = new CMyData();
+                data.Color = CurrentPenColor;
+                data.Width = iCurrentPenWidth;
+                //ar = new ArrayList();
+                //ar.Add(new Point(x, y));
+                data.AR.Add(new Point(x, y));
+            }
+            doubleclick = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Capture && e.Button == MouseButtons.Left)
+            {
+                Graphics G = CreateGraphics();
+                //펜 설정하기(색깔과 굵기) 
+                Pen p = new Pen(data.Color, data.Width);
+                G.DrawLine(p, x, y, e.X, e.Y);
+                x = e.X;
+                y = e.Y;
+                data.AR.Add(new Point(x, y));
+                G.Dispose();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(doubleclick==true)
+            {
+                total_lines.AddLast(data);
+                doubleclick = false;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (CMyData line in total_lines)
+            {
+                Pen p = new Pen(line.Color, line.Width);
+                for (int i = 1; i < line.AR.Count; i++)
+                {
+                    e.Graphics.DrawLine(p, (Point)line.AR[i - 1], (Point)line.AR[i]);
+                }
+            }
+        }
+
+        private void 대화상자ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dlg = new Form2();
+            dlg.DialogPenColor = CurrentPenColor;  //set
+            dlg.iDialogPenWidth = iCurrentPenWidth;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                CurrentPenColor = dlg.DialogPenColor;      //get
+                iCurrentPenWidth = dlg.iDialogPenWidth;
+            }
+            dlg.Dispose();
+
+        }
+
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "텍스트 파일|*.bin|모든 파일|*.*";//* Filter 추가 
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Create, FileAccess.Write);//*원래 경로가 위처럼됐는데 이렇게 변경
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, total_lines);
+                //fs.Write(data, 0, data.Length);//* 쓸 때는 인자를 byte 형 하나 쓰고, 0부터 쓰고, data의 길이만큼
+                fs.Close();
+                MessageBox.Show(saveFileDialog1.FileName + "의 파일에 기록했습니다");
+            }
+        }
+
+        private void 불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.Filter = "텍스트 파일|*.bin|모든 파일|*.*";//*Filter 추가 
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //MessageBox.Show(openFileDialog1.FileName + "를 선택했습니다");
+                    FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.Read);
+                    BinaryFormatter bf = new BinaryFormatter();
+                    total_lines = (LinkedList<CMyData>)bf.Deserialize(fs);
+                    //fs.Read(data, 0, data.Length);//*불러올때는 Read로 열고 첫번째부터 쓰겠다, data.Length 즉 8byte 만큼 읽어들이겠다
+                    fs.Close();
+                    //MessageBox.Show(openFileDialog1.FileName + "를 선택했습니다\n");
+                    Invalidate();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("지정한 파일이 없습니다.");
+            }
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CurrentPenColor = Color.Red;
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CurrentPenColor = Color.Green;
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CurrentPenColor = Color.Blue;
+        }
+
+    }
+    [Serializable]
+    class CMyData
+    {
+        private ArrayList Ar;
+
+        public CMyData()  //생성자함수
+        {
+            Ar = new ArrayList();
+        }
+        public Color Color
+        {
+            get;
+            set;
+        }
+        public int Width
+        {
+            get;
+            set;
+        }
+        public ArrayList AR
+        {
+            get { return Ar; }
+        }
+    }
+}
---
+++ Week05_1/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLines
+{
+    public partial class Form2 : Form
+    {
+        public Color DialogPenColor { get; set; }
+        public int iDialogPenWidth { get; set; }
+
+   
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            iDialogPenWidth = (((ComboBox)sender).SelectedIndex + 1) * 2;
+            label5.Invalidate();//정보가 변경되어서 그려져야 하는 부분에다가 label5.Paint 메소드를 계속 뿌려줘 라는 의미 
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            for (int i = 2; i <= 10; i += 2)
+            {
+                comboBox1.Items.Add(i);
+            }
+            //comboBox1.SelectedIndex = iDialogPenWidth / 2 - 1;
+            comboBox1.Text = iDialogPenWidth.ToString();
+
+            hScrollBar1.Value = DialogPenColor.R;
+            hScrollBar2.Value = DialogPenColor.G;
+            hScrollBar3.Value = DialogPenColor.B;
+            label2.Text = "R";
+            label3.Text = "G";
+            label4.Text = "B";
+            textBox1.Text = DialogPenColor.R.ToString();
+            textBox2.Text = DialogPenColor.G.ToString();
+            textBox3.Text = DialogPenColor.B.ToString();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            iDialogPenWidth = int.Parse(comboBox1.Text);
+            label5.Invalidate();//정보가 변경되어서 그려져야 하는 부분에다가 label5.Paint 메소드를 계속 뿌려줘 라는 의미 
+        }
+
+
+        private void hScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            DialogPenColor = Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+            textBox1.Text = hScrollBar1.Value.ToString();
+            textBox2.Text = hScrollBar2.Value.ToString();
+            textBox3.Text = hScrollBar3.Value.ToString();
+            label5.Invalidate();//정보가 변경되어서 그려져야 하는 부분에다가 label5.Paint 메소드를 계속 뿌려줘 라는 의미 
+        }
+
+        private void label5_Paint(object sender, PaintEventArgs e)//스스로 지 window 에다가 그린다는 의미 
+        {
+            e.Graphics.DrawLine(new Pen(DialogPenColor, iDialogPenWidth), 0, label5.Height / 2, 
+                label5.Width, label5.Height / 2);
+            //여기 PAINT로 그리는 로직을 이용해서 label5.invalidate 즉 다시 Paint 해야할때마다 다시 그려줘 라는 메시지를 전달해주면 계속해서 다시그려짐
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            hScrollBar1.Value = int.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            hScrollBar2.Value = int.Parse(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            hScrollBar3.Value = int.Parse(textBox3.Text);
+        }
+    }
+}
